296b1e1396b78c6fc5ea93efc4dbade80297a582
Gustavo Salinas <gustavosr450@gmail.com>
2025-03-27 22:41:56 -0600

    commit v2

--- src/main/java/mx/tecnm/toluca/config/Config.java
+++ src/main/java/mx/tecnm/toluca/config/Config.java
@@ -1,8 +1,10 @@
 package mx.tecnm.toluca.config;
 
 public class Config {
-    public static final String DATABASE_SERVICE_URL = "http://192.168.1.85:8080/DatabaseService"; // Reemplazar con la URL real
+    public static final String DATABASE_SERVICE_URL = "https://c0c6-2806-104e-16-1f1-12e1-6efa-4429-523f.ngrok-free.app/DatabaseService"; // Reemplazar con la URL real
     public static final String FILE_SERVER_URL = "https://filesystembk.s3.us-east-2.amazonaws.com"; // Reemplazar con la URL real
     public static final String DATABASE_EMAIL = "proveedor1@gmail.com"; // Reemplazar con el correo real
     public static final String DATABASE_PASSWORD = "123456"; // Reemplazar con la contraseña real
 }
+
+///https://c0c6-2806-104e-16-1f1-12e1-6efa-4429-523f.ngrok-free.app/DatabaseService
